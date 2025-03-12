3a0471ef4787ee560fde032838340058c314814d
t-tojiboyeva <126105745+t-tojiboyeva@users.noreply.github.com>
2025-03-12 15:35:08 +0300

    Create strings.py

---
+++ lesson-1/homework/strings.py
@@ -0,0 +1,35 @@
+Homework:
+
+1.Given a side of square. Find its perimeter and area.
+a=float(input("Enter value  for square"))
+P=float(4*a)
+S=a*2
+print(P)
+print(S)
+
+2.Given diameter of circle. Find its length.
+import math
+d=float(input("Enter diameter for circle"))
+L=d*math.pi
+print(L)
+
+3.Given two numbers a and b. Find their mean.
+
+a=int(input("Enter a values:"))
+b=int(input("Enter b values:"))
+M=(a+b)/2
+print(M)
+
+4.Given two numbers a and b. Find their sum, product and square of each number.
+import math
+a=float(input("Enter a values:"))
+b=float(input("Enter b values:"))
+Sum=a+b
+Product=a*b
+Root1=math.sqrt(a)
+Root2=math.sqrt(b)
+print(f" Sum of a and b is {Sum}")
+print(f" Product of a and b is {Product}")
+print(f" Root of a  is {Root1}")
+print(f" Root of b  is {Root2}")
+
